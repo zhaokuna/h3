d064455cc74b49ab97a454e2ea5e23cd53dd6d1c
xujunzhaokuna <2668828379@qq.com>
2019-11-28 17:19:10 +0800

    zhaokun

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <Redirect></Redirect>
+    <Dashboard></Dashboard>
   </div>
 </template>
 
 <script>
 import Redirect from "../components/redirect";
+import Dashboard from "../components/dashboard";
 export default {
   components: {
-    Redirect
+    Redirect,
+    Dashboard
   }
 };
 </script>
